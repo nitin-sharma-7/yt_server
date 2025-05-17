c1a255bea117f4561d900d2c6f3eedfd0fa92d75
nitin-sharma-7 <ns17740654@gmail.com>
2025-05-17 17:58:55 +0530

    not valid cred

--- middlewares/userMiddlewares/userVerify.js
+++ middlewares/userMiddlewares/userVerify.js
@@ -8,8 +8,7 @@ const userVerify = async (req, res, next) => {
     const data = await userModel.find({ username }).populate("channel");
     // const data = await userModel.find({ username });
     if (!data || data.length == 0) {
-      // return res.status(400).json({ messsage: "user not found" });
-      return next("invalid crediantials");
+      return res.status(400).json({ messsage: "user not found" });
     }
     const [newuser] = data;
     const validation = bcrypt.compareSync(password, newuser.password);
