80dd51972ac44b6ff274ff1d30c7250aa21dcdbd
nitin-sharma-7 <ns17740654@gmail.com>
2025-05-17 17:57:02 +0530

    eror handle at invalid credentials

--- middlewares/userMiddlewares/userVerify.js
+++ middlewares/userMiddlewares/userVerify.js
@@ -4,20 +4,12 @@ import { generateJWT } from "../../jwt/userJWT.js";
 import bcrypt from "bcrypt";
 const userVerify = async (req, res, next) => {
   try {
-    const errors = [];
     const { username, password } = req.body;
-
-    if (!username) return errors.push("username required");
-    if (!password) return errors.push("password required");
-
-    if (errors.length > 0) {
-      return res.status(400).json({ errors });
-    }
-
     const data = await userModel.find({ username }).populate("channel");
     // const data = await userModel.find({ username });
     if (!data || data.length == 0) {
-      return res.status(400).json({ messsage: "user not found" });
+      // return res.status(400).json({ messsage: "user not found" });
+      return next("invalid crediantials");
     }
     const [newuser] = data;
     const validation = bcrypt.compareSync(password, newuser.password);
